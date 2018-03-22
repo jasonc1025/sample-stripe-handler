f384fe43b7503df79b3aa0ec1c9c9ed96c7a8e05
jasonc1025 <jasonc@eande.world>
2018-03-22 00:02:12 -0700

    'Stripe' Secret-Key Customization

--- stripe-handler/index.js
+++ stripe-handler/index.js
@@ -1,4 +1,5 @@
-var stripe = require('stripe')('sk_test_js5LRkmS7OsYHtVc7XYvK9OB');
+// [jwc]+1 orig: var stripe = require('stripe')('sk_test_js5LRkmS7OsYHtVc7XYvK9OB');
+var stripe = require('stripe')('sk_test_NhYGHM4AS0f4qmPaxknfR9fU');
 // ^ this is a stripe testing key
 
 module.exports = function(context, req) {
